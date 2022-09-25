7735003daffb89940b8342c716d797b47b513c12
Canberk Aydemir <canberk.aydemir@yahoo.com>
2022-09-25 13:53:07 +0300

    Added comments to ether-store.sol.

--- Ether-Store/ether-store.sol
+++ Ether-Store/ether-store.sol
@@ -3,23 +3,30 @@
 pragma solidity ^0.8.7;
 
 contract store {
-    address public owner;
-    uint public balance;
+    address public owner; // Field for storing owner of the smart contract
+    uint public balance; // Field for storing the current balance of the owner
 
     constructor(){
+        // Very basic constructor for initializing the owner field with the sender property of message.
         owner = msg.sender;
     }
 
     function getFee() payable external {
+        // Basic deposit function used for depositing some amount to owner
         balance += msg.value;
     }
 
     function withdraw(uint amount, address payable destination) public {
-        require(msg.sender == owner, "Only the owner can withdraw!");
-        require(balance >= amount, "Not enough credits!");
+        // Basic function for withdrawing a specific amount and sending it to another address
 
-        destination.transfer(amount);
-        balance -= amount;
+        require(msg.sender == owner, "Only the owner can withdraw!");
+        // Make sure that the withdraw takes place from the owner  
+
+        require(balance >= amount, "Not enough credits!");
+        // Make sure there is enough amount in the owner's balance
+
+        destination.transfer(amount); // Send the specified amount to the destination address
+        balance -= amount; // Update balance
 
     }
 
